852daeb76aec441c6a38d1116233f4937be76aeb
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-10-25 20:25:08 +0100

    Add project files.

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        return Created();
+    }
+}
