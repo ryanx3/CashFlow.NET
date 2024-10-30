bc54a65f442183731a41da15b4b2ab462241eaf7
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-10-30 21:04:28 +0000

    feat: added first unit tests with Success.

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpensesValidatorTests
+{
+
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Lorem ipsum",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard,
+            Title = "Title"
+        };
+
+        var response = validator.Validate(request);
+
+        Assert.True(response.IsValid);
+    }
+}
