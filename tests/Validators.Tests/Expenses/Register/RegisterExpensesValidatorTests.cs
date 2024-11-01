539ec7dbfa269fadda8209235da61d45580651eb
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-11-01 19:38:09 +0000

    feat: create first entitie "Expense" to database

--- tests/Validators.Tests/Expenses/Register/RegisterExpensesValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
@@ -34,5 +35,52 @@ public class RegisterExpensesValidatorTests
         response.IsValid.Should().BeFalse();
         response.Errors.Should().ContainSingle().
             And.Contain(error => error.ErrorMessage == ResourceErrorMessages.TITLE_REQUIRED);
+    }
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        var response = validator.Validate(request);
+
+        response.IsValid.Should().BeFalse();
+        response.Errors.Should().ContainSingle().
+            And.Contain(error => error.ErrorMessage == ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
+    }
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        request.PaymentType = (PaymentType)333;
+
+        var response = validator.Validate(request);
+
+        response.IsValid.Should().BeFalse();
+        response.Errors.Should().ContainSingle().
+            And.Contain(error => error.ErrorMessage == ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        request.Amount = amount;
+
+        var response = validator.Validate(request);
+
+        response.IsValid.Should().BeFalse();
+        response.Errors.Should().ContainSingle().
+            And.Contain(error => error.ErrorMessage == ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
     }
 }
