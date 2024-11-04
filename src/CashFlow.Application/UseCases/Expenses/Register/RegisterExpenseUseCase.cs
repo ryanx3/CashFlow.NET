b84ec15dae48588cc5d3ca9680ca602f350ec7df
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-11-04 18:46:21 +0000

    feat: added db entity to ExpenseUseCase

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -9,6 +10,15 @@ public class RegisterExpenseUseCase
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
+
+        var entity = new Expense {
+            Title = request.Title,
+            Description = request.Description,
+            Amount  = request.Amount,
+            Date = request.Date,
+            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
+        };
+
         return new ResponseRegisteredExpenseJson();
     }
 
