5e19ef948136c1b23ad2d11416b0f002ccc54865
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-11-05 21:44:46 +0000

    feat: added dbContext as a injection dependency

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpenseRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpenseRepository.cs
@@ -4,10 +4,14 @@ using CashFlow.Domain.Repositories.Expenses;
 namespace CashFlow.Infrastructure.DataAccess.Repositories;
 internal class ExpenseRepository : IExpensesRepository
 {
+    private readonly CashFlowDbContext _dbContext;
+    public ExpenseRepository(CashFlowDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public void Add(Expense expense)
     {
-        var dbContext = new CashFlowDbContext();
-        dbContext.Expenses.Add(expense);
-        dbContext.SaveChanges();
+        _dbContext.Expenses.Add(expense);
+        _dbContext.SaveChanges();
     }
 }
