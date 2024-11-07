25b54fb86e175f612d8a75477b331cefd9771db1
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-11-07 22:42:06 +0000

    feat: added UnitOfWork to save changes in database

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionsBase;
 
@@ -8,26 +9,7 @@ namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository;
-    public RegisterExpenseUseCase(IExpensesRepository repository)
-    {
-        _repository = repository;
-    }
-    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
-    {
-        Validate(request);
-
-        var entity = new Expense {
-            Title = request.Title,
-            Description = request.Description,
-            Amount  = request.Amount,
-            Date = request.Date,
-            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
-        };
-
-        _repository.Add(entity);
-
-        return new ResponseRegisteredExpenseJson();
-    }
+    
 
     private void Validate(RequestRegisterExpenseJson request)
     {
---
+++ src/CashFlow.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Repositories;
+using CashFlow.Infrastructure.DataAccess;
+
+namespace CashFlow.Infrastructure;
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly CashFlowDbContext _dbContext;
+    public UnitOfWork(CashFlowDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Commit()
+    {
+       _dbContext.SaveChanges();
+    }
+}
