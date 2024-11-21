753c68ccd6d2f3af27d7098d0684420eb2e5c73c
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2024-11-21 17:02:46 +0000

    refactor: exception handling to unify structure and simplify processing

--- src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -1,8 +1,11 @@
 ﻿namespace CashFlow.Exception.ExceptionsBase;
 public abstract class CashFlowException : System.Exception
 {
-    protected CashFlowException(string message) : base(message) 
+    protected CashFlowException(string message) : base(message)
     {
-        
+
     }
+
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
 }
--- src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ src/CashFlow.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,10 +1,19 @@
-﻿namespace CashFlow.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase;
 public class ErrorOnValidationException : CashFlowException
 {
-    public List<string> Errors { get; set; }
+    public List<string> _errors{ get; set; }
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
 
     public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
     {
-        Errors = errorMessages;
+        _errors = errorMessages;
+    }
+
+    public override List<string> GetErrors()
+    {
+       return _errors;
     }
 }
